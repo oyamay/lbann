1cee7be022f829d51696a1a2a321fa84d2da68a4
Yosuke Oyama <17844184+oyamay@users.noreply.github.com>
2019-02-06 15:02:13 -0800

    Add config_generator.py

---
+++ scripts/proto/lbann/config_generator.py
@@ -0,0 +1,43 @@
+import sys
+import argparse
+
+import lbann.proto as lp
+
+def generate_config(model, data_reader, optimizer):
+    """
+    A wrapper function for lp.save_prototext.
+    This function save a prototext to a path given via --prototext-path.
+    This argument might be implicitly set by job-submission scripts.
+
+    Users can use custom arguments as long as the arguments are parsed
+    with `parse_known_args`:
+    ```
+    import argparse
+    import lbann.prototext_generator as lpg
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--model-arg", action="store", type=int)
+        args,_ = parser.parse_known_args()
+
+        lpg.save_prototext(
+            create_model(args.model_arg),
+            create_data_reader(),
+            create_optimizer())
+    ```
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--prototext-path", type=str,
+        help="Path to output a prototext",
+        required=True)
+
+    args, _ = parser.parse_known_args()
+
+    lp.save_prototext(
+        args.prototext_path,
+        model=model,
+        data_reader=data_reader,
+        optimizer=optimizer)
