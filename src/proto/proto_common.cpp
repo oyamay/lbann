2ec0161e24d72d3c0dbb47e6bccf40bd623f0b83
Yosuke Oyama <17844184+oyamay@users.noreply.github.com>
2019-04-08 00:49:44 -0700

    Substitute environment variables in data_file_pattern

--- src/proto/proto_common.cpp
+++ src/proto/proto_common.cpp
@@ -14,6 +14,7 @@
 #include <unordered_set>
 #include <sys/stat.h>
 #include <cassert>
+#include <regex>
 
 namespace lbann {
 
@@ -201,7 +202,23 @@ void init_data_readers(lbann_comm *comm, const lbann_data::LbannPB& p, std::map<
       if(!endsWith(filedir, "/")) {
         filedir = filedir + "/";
       }
-      const auto paths = glob(filedir + readme.data_file_pattern());
+      auto dirname = filedir + readme.data_file_pattern();
+      { // Replace ${*} with the corresponding environemnt variable
+       std::regex re("(.*)\\$\\{([^}]+)\\}(.*)");
+       std::smatch match;
+       while(regex_match(dirname, match, re)) {
+         const std::string head    = match[1];
+         const std::string envname = match[2];
+         const std::string tail    = match[3];
+         const char *env = std::getenv(envname.c_str());
+         assert(c != nullptr);
+         dirname = head + std::string(env) + tail;
+         if(master) {
+           std::cout << "dirname: " << dirname << std::endl;
+         }
+       }
+      }
+      const auto paths = glob(dirname);
       reader_cosmoflow->set_npz_paths(paths);
       reader_cosmoflow->set_scaling_factor_int16(readme.scaling_factor_int16());
       reader = reader_cosmoflow;
